ddc0c37b07c0dbdeb532b7e27ab83e31132395cc
Chidi Onyekwelu <chidixonyekwelu@fb.com>
2022-06-28 16:04:15 -0700

    Login Feature

--- Instagram/LoginViewController.m
+++ Instagram/LoginViewController.m
@@ -6,18 +6,128 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
+- (IBAction)signupButton:(id)sender;
+
+- (IBAction)loginButton:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextField *usersPassword;
+@property (weak, nonatomic) IBOutlet UITextField *usersUsername;
+
+
 
 @end
 
 @implementation LoginViewController
+- (IBAction)loginButton:(id)sender {
+    if ([self.usersUsername.text isEqual:@""] || [self.usersPassword.text isEqual:@""]){
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                                               message:@"Message"
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    // create a cancel action
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle cancel response here. Doing nothing will dismiss the view.
+                                                      }];
+    // add the cancel action to the alertController
+    [alert addAction:cancelAction];
 
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+    }
+    else{
+            [self loginUser];
+        }
+}
+
+
+
+- (IBAction)signupButton:(id)sender {
+    
+    if ([self.usersUsername.text isEqual:@""] || [self.usersPassword.text isEqual:@""]) {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                                               message:@"Message"
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    // create a cancel action
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle cancel response here. Doing nothing will dismiss the view.
+                                                      }];
+    // add the cancel action to the alertController
+    [alert addAction:cancelAction];
+
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+}
+    else{
+            [self registerUser];
+        }
+
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usersUsername.text;
+    newUser.password = self.usersPassword.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"FirstSegue" sender:nil];
+            // manually segue to logged in view
+        }
+    }];
+}
+
+- (void)loginUser {
+    NSString *username = self.usersUsername.text;
+    NSString *password = self.usersPassword.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"FirstSegue" sender:nil];
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
 /*
 #pragma mark - Navigation
 
@@ -28,4 +138,7 @@
 }
 */
 
+
+
 @end
+

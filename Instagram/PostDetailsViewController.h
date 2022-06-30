909182eea900c9c2e5cd615a42ddb62dfbf5ca24
Chidi Onyekwelu <chidixonyekwelu@fb.com>
2022-06-30 16:55:10 -0700

    Committed

---
+++ Instagram/PostDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  PostDetailsViewController.h
+//  Instagram
+//
+//  Created by Chidi Onyekwelu on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

312311de55b828c982dfad3c5195f00ad4c6dddf
Chidi Onyekwelu <chidixonyekwelu@fb.com>
2022-07-15 00:17:10 -0700

    Updated

--- Instagram/View Controllers/HomeFeedViewController.m
+++ Instagram/View Controllers/HomeFeedViewController.m
@@ -18,16 +18,14 @@
 @interface HomeFeedViewController () <ComposeViewControllerDelegate ,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *arrayOfposts;
-@property(strong, nonatomic) UIRefreshControl *refreshcontrol;
+@property(strong, nonatomic) UIRefreshControl *refreshControl;
 
 @end
 
 @implementation HomeFeedViewController
 - (IBAction)didTapLogout:(id)sender {
         
-    // PFUser.current() will now be nil
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
-        // PFUser.current() will now be nilUIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
         LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
         SceneDelegate *mySceneDelegate = (SceneDelegate *) UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
@@ -42,11 +40,10 @@
     
     self.tableView.rowHeight = UITableViewAutomaticDimension;
     [self fetchData];
-    self.refreshcontrol = [[UIRefreshControl alloc] init];
-    [self.refreshcontrol addTarget:self action:@selector(fetchData) forControlEvents:UIControlEventValueChanged];
-    [self.tableView insertSubview:self.refreshcontrol atIndex:0];
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchData) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
  
-    // Do any additional setup after loading the view.
 }
 - (void)beginRefresh:(UIRefreshControl *)refreshControl {
     [self.tableView reloadData];
@@ -73,13 +70,11 @@
     PFQuery *query = [PFQuery queryWithClassName:@"Post"];
     [query orderByDescending:@"createdAt"];
     query.limit = 20;
-    // fetch data asynchronously
     [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
         if (posts != nil) {
-            // do something with the array of object returned by the call
             self.arrayOfposts = (NSMutableArray*) posts;
             [self.tableView reloadData];
-            [self.refreshcontrol endRefreshing];
+            [self.refreshControl endRefreshing];
         } else {
             NSLog(@"%@", error.localizedDescription);
         }
@@ -88,15 +83,14 @@
 
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"PostDetailsSegue"]) {
         Post * post = self.arrayOfposts[[self.tableView indexPathForCell:sender].row
         ];
-        PostDetailsViewController *detailsviewcontroller = [segue destinationViewController];
-        detailsviewcontroller.post = post;
+        PostDetailsViewController *detailsViewController = [segue destinationViewController];
+        detailsViewController.post = post;
             
-    //     Get the new view controller using [segue destinationViewController].
+  
     }
     else {
         UINavigationController *navigationcontroller = [segue destinationViewController];
